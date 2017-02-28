8892dfb4dbc239e73693a3098d24eb832c4d99f2
renanpvaz <renanp.vaz@outlook.com>
2017-02-28 14:37:12 -0300

    use '\r\n' when windows

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
 
 const program = require('commander');
 const colors = require('colors');
+const newline = /^win/.test(process.platform) ? '\r\n' : '\n';
 
 const action = text => {
   const titleWords = text
     .split('')
     .map(
       word => {
-        const titleWord = characters[word.toUpperCase()].split('\n');
+        const titleWord = characters[word.toUpperCase()].split(newline);
 
         titleWord.shift();
         titleWord.pop();
@@ -25,7 +26,7 @@ const action = text => {
       (line, i) => titleWords.map(words => words[i]).join('').replace(/ /g, '')
     );
 
-  const title = `\n${lines.join('\n').replace(/0/g, ' ')}\n`;
+  const title = newline + lines.join(newline).replace(/0/g, ' ') + newline;
 
   console.log(title[program.style] || title['white']);
 };
